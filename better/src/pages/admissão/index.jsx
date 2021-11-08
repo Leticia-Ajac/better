ca0a1cb4a8b07a8732d7adac09ffe4fe47e3a7dd
joao <joaofgamba@gmail.com>
2021-11-08 15:32:09 -0300

    Header adimissao

--- better/src/pages/admissão/index.jsx
+++ better/src/pages/admissão/index.jsx
@@ -1,11 +1,14 @@
 import '../home/styles.css';
+import './admissao.css';
 import logo_branca from '../../assets/logo-branca.png'; 
 import { Link } from 'react-router-dom';
+import criançasadmissão from '../../assets/criancas-admissao.png';
+import imgtitulo from '../../assets/div-titulo.png';
+import imgtitulo2 from '../../assets/div-titulo2.png';
 
 const adimissão = () => {
     return (
-        <div id='page-home'>
-
+        <div id='admissão'>
             <header>
                 
                 <Link to="/"><img class="topicos" id="logogerminare" src={logo_branca} alt="logo germinare"/></Link>
@@ -30,7 +33,21 @@ const adimissão = () => {
                 </ul>
 
             </header>
+
+
+
+        <div id='miniHeader'>
+            <img id="tituloimg" src={imgtitulo}/>
+            <h1 id="titulo">Processo de Adimissão</h1>
+            <img id="tituloimg2" src={imgtitulo2}/>
         </div>
+        <div>
+          <img className="criancasAdimissao" src={criançasadmissão}/>
+          <p>A lista de convocados para a segunda etapa do 
+             <spam class="toBold">Processo de Admissão está disponível!</spam></p>
+        </div>
+        <button>Veja os resultados</button>
+    </div>
     );
 };
 
