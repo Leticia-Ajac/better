409f30c35059bf2ee8ab8e8132c0d8ad3434869c
joao <joaofgamba@gmail.com>
2021-11-12 10:43:26 -0300

    ajuste de erros de portugues

--- better/src/pages/header/header.jsx
+++ better/src/pages/header/header.jsx
@@ -1,29 +1,26 @@
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo_branca from '../../assets/logo-branca.png'
-import {TiThMenu} from 'react-icons/ti';
+import { TiThMenu } from 'react-icons/ti';
 import './header.css';
-import {AiFillContacts} from 'react-icons/ai'
+import { AiFillContacts } from 'react-icons/ai'
 
 const header = () => {
-    return(
+    return (
         <div id='page-home'>
             <header>
-                <Link to ='/'><img id="logogerminare" src={logo_branca} alt="logo germinare"/></Link>
+                <Link to='/'><img id="logogerminare" src={logo_branca} alt="logo germinare" /></Link>
                 <ul id="listatopicos">
-                <div>
-                    <div className='burger' id='hamburguer'>
-                    <label for='icone'>
-                        <p class='icons'>
-                            <TiThMenu />
-                        </p>    
-                    </label>       
+                    <div>
+                        <div className='burger' id='hamburguer'>
+                            <label for='icone'>
+                                <p class='icons'>
+                                    <TiThMenu color="white" />
+                                </p>
+                            </label>
+                        </div>
                     </div>
-                </div>
-
-
-                
-                    <Link to="/admissão" onClick={()=>{if (document.readyState === 'complete') {window.scroll(0, 0)}}}>
+                    <Link to="/admissão" onClick={() => { if (document.readyState === 'complete') { window.scroll(0, 0) } }}>
                         <li class="topicos">Admissão</li>
                     </Link>
                     <Link>
@@ -31,11 +28,11 @@ const header = () => {
                     </Link>
                     <Link>
                         <li class="topicos">Nossa Cultura</li>
-                    </Link>   
-                    <Link>
-                        <li class="topicos">Novidades</li> 
                     </Link>
-                    <Link to='' class='topicos' id="portal_aluno"style={{width: '200px'}}>
+                    <Link>
+                        <li class="topicos">Novidades</li>
+                    </Link>
+                    <Link to='' class='topicos' id="portal_aluno" style={{ width: '200px' }}>
                         <li>Portal do aluno</li>
                         <div class='nicks'>
                             <p>Sophia Escolar</p>
@@ -46,26 +43,26 @@ const header = () => {
                     </Link>
                 </ul>
             </header>
-            <input type="checkbox" id='icone' className='trigger-input'/>
+            <input type="checkbox" id='icone' className='trigger-input' />
             <div className='nome'>
-                <Link to ='/admissao'onClick={()=>{if (document.readyState === 'complete') {window.scroll(0, 0)}}} >
+                <Link to='/admissao' onClick={() => { if (document.readyState === 'complete') { window.scroll(0, 0) } }} >
                     <p>Admissão</p>
                 </Link>
-                <Link to =''>
+                <Link to=''>
                     <p>Academias</p>
                 </Link>
-                <Link to =''>
+                <Link to=''>
                     <p>Nossa Cultura</p>
                 </Link>
-                <Link to =''>
+                <Link to=''>
                     <p>Novidades</p>
                 </Link>
-                <Link to =''>
+                <Link to=''>
                     <p>Portal do Aluno</p>
                 </Link>
-                </div>   
+            </div>
         </div>
-);
+    );
 };
 
 
