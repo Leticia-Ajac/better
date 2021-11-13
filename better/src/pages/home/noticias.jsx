c38b3ea0d79a389fa44cf0f01c4055aed5f1f134
ajac <leticiajac2018@gmail.com>
2021-11-13 01:24:39 -0300

    responsividade noticias + little destaque

--- better/src/pages/home/noticias.jsx
+++ better/src/pages/home/noticias.jsx
@@ -4,7 +4,12 @@ import firstcard2_img from '../../assets/firstcard_img2.png'
 import firstcard3_img from '../../assets/firstcard_img3.png'
 import firstcard4_img from '../../assets/firstcard_img4.png'
 import secondcard_img from '../../assets/secondcard_img.png'
+import secondcard2_img from '../../assets/secondcard_img2.png'
 import secondcard4_img from '../../assets/secondcard_img4.png'
+import thirdcard_img from '../../assets/thirdcard_img.png'
+import thirdcard2_img from '../../assets/thirdcard_img2.png'
+import thirdcard3_img from '../../assets/thirdcard_img3.png'
+import thirdcard4_img from '../../assets/thirdcard_img4.png'
 
 import './styles/noticias.css';
 
@@ -32,14 +37,22 @@ const Noticias = () => {
                     <p>Geovanna, ex germinare, no Canal Runal, confira!</p>
                     <picture>
                         <source media="(max-width: 480px" srcSet={secondcard4_img} />
+                        <source media="(max-width: 769px)" srcSet={secondcard_img} />
+                        <source media="(min-width: 1024px)" srcSet={secondcard_img} />
+                        <source media="(max-width: 1023px)" srcSet={secondcard2_img} />
+
                         <img src={secondcard_img}/>
                     </picture>
                 </div>
                 <div className="card-grid">
-                    <p>Geovanna, ex germinare, no Canal Runal, confira!</p>
+                    <p>Agora a Germinare tem o Ensino Tech! </p>
                     <picture>
-                        <source media="(max-width: 480px" srcSet={secondcard4_img} />
-                        <img src={secondcard_img}/>
+                        <source media="(max-width: 480px" srcSet={thirdcard4_img} />
+                        <source media="(max-width: 769px)" srcSet={thirdcard3_img} />
+                        <source media="(min-width: 1024px)" srcSet={thirdcard_img} />
+                        <source media="(max-width: 1023px)" srcSet={thirdcard2_img} />
+
+                        <img src={thirdcard4_img}/>
                     </picture>
                 </div>
             </div>
