165f346ed25c0cff7bca4dde83c77f953e7d6da5
joao <joaofgamba@gmail.com>
2021-11-14 00:54:22 -0300

    academias responsiva

--- better/src/pages/home/academias.jsx
+++ better/src/pages/home/academias.jsx
@@ -1,65 +1,44 @@
 import L_picpay from '../../assets/picpay.png';
 import L_original from '../../assets/original.png';
 import L_seara from '../../assets/seara.png';
-import L_flora from '../../assets/flora.png';
 import L_swift from '../../assets/swift.png';
 import L_friboi from '../../assets/friboi.png';
-
-import linha from '../../assets/linha.png';
-import linha2 from '../../assets/linha2.png';
-
-import "./styles/academias.css"
-
+import './styles/academias.css';
 
 const Academias = () => {
     return (
-        <div className='academias'>
+        <div id='parte-academias'>
             <h1>ACADEMIAS</h1>
-            <img src={linha} alt="linha azul" id="linha-1" className="linhas"/>
-            <div id="parent">
-                <div className="cards-academias">
-                    
-                    
-                        <div className="card" id="picpay">
-                            <div className="format-card">
-                                <img src={L_picpay} alt="picpay"/>
-                                <p>Nossa Academia de Tecnologia foi desenvolvida para atender a demanda dos alunos e do mercado de trabalho atual em relação ao desenvolvimento, pesquisa e aplicação de tecnologias.</p>
-                            </div>  
+            <div id='quebrar-page'>
+                <h3>Conheça nosso modelo de ensino:</h3>
+                <p>Dividido em academias que separam os aprendizados em módulos de conhecimento, a Germinare propõe aos estudantes se aventurarem no mundo dos negócios.</p>
+                    <div id='cards-academias'>
+                        <div className='card-academia' id='picpay'>
+                            <p>PicPay</p>
+                            <h6>Nossa academia de tecnologia desafia os alunos a entrarem de cabeça no mundo os computadores e descobrir as maravilhas da programação 🚀.</h6>
+                            <h6>Eai, Bora codar?</h6>
                         </div>
-
-                        <div className="card" id="original">
-                            <div className="format-card">
-                                <img src={L_original} alt="original"/>
-                                <p>A Academia de Finanças foi criada para formar profissionais com alto nivel de conhecimento contábil e econômico, principalmente através de atividades práticas na bolsa de valores.</p>
-                            </div>
-                            
+                        <div className='card-academia' id='original'>
+                            <p>Original</p>
+                            <h6>Nossa academia de finanças desperta o interesse no mercado financeiro e engaja nossos alunos a se tornarem experts em finanças.</h6>
+                            <h6>Partiu trade!!!</h6>
                         </div>
-
-                        <div className="card" id="seara">
-                            <div className="format-card">
-                                <img src={L_seara} alt="seara" id="img-seara"/>
-                                <p>A Academia tem como objetivo formar profissionais ligados ao varejo.</p>
-                            </div>
-                            
-                            
+                        <div className='card-academia' id='seara'>
+                            <p>Seara</p>
+                            <h6>Nossa academia Seara torna os alunos estrategistas e planejadores, não há contratempo que segure esses gênios.</h6>
+                            <h6>É hora planejar o seu futuro.</h6>
                         </div>
-                    
-
-                
-                        <div className="card" id="swift">
-                            <div className="format-card">
-                                <img src={L_swift} alt="swift"/>
-                                <p>A Academia de Bens de Consumo busca formar tocadores de negócios, propondo como instrumento de aprendizagem o entendimento da estrutura organizacional e conhecimento de mercado.</p>
-                            </div>
+                        <div className='card-academia' id='friboi'>
+                            <p>Friboi</p>
+                            <h6>Nossa academia Friboi torna acessível aos estudantes o entedimento completo da cadeia de produção do nosso grupo.</h6>
+                            <h6>Engaje no negócio com a gente!</h6>
                         </div>
-                        <div className="card" id="friboi">
-                            <div className="format-card">
-                                <img src={L_friboi} alt="friboi"/>
-                                <p>A Academia Friboi tem como objetivo fomentar a curiosidade dos alunos em relação aos processos produtivos e a cadeia de garantia da qualidade.</p>
-                            </div>
+                        <div className='card-academia' id='swift'>
+                            <p>Swift</p>
+                            <h6>A academia Swift transforma os estudantes em vendedores natos, nossa tarefa é preparar pessoas carismáticas e que fazem a diferença no ambiente o qual estão </h6>
+                            <h6>Vamos começar?</h6>
                         </div>
-                </div>
-                <img src={linha2} alt="linha azul" className="linhas" id="linha-2"/>
+                    </div>
             </div>
         </div>
     )
