eb9bfd8eab8e08dc5d3ea65c3b4cf0eef34187de
ajac <leticiajac2018@gmail.com>
2021-11-16 15:30:43 -0300

    html linha do tempo

---
+++ better/src/pages/qulture/contents/linha-tempo.jsx
@@ -0,0 +1,48 @@
+
+
+const Linha_Tempo = () => {
+    return (
+        <div className="container-linha">
+            <div className="titulo-linhat">
+                <h1>LINHA DO TEMPO</h1>
+            </div>
+            <div className="container-desenho">
+                <div className="linha-cima">
+                    <div className="ano-topic">
+                        <p className="ano">2010</p>
+                        <p className="topic">Escola de Empreendedorismo</p>
+                    </div>
+                    <div className="ano-topic">
+                        <p className="ano">2016</p>
+                        <p className="topic">Programa de Estágios</p>
+                    </div>
+                    <div className="ano-topic">
+                        <p className="ano">2019</p>
+                        <p className="topic">Estruturação dos Pilares de Negócio</p>
+                    </div>
+                </div>
+                <picture>
+                    <source media="()" srcset="" />
+                    <source media="()" srcset="" />
+                    <source media="()" srcset="" />
+                    <source media="()" srcset="" />
+                    <img />
+                </picture>
+                <div className="linha-baixo">
+                    <div className="ano-topic">
+                        <p className="ano">2014/2015</p>
+                        <p className="topic">Escola Técnica em Admistração de Empresas</p>
+                    </div>
+                    <div className="ano-topic">
+                        <p className="ano">2017</p>
+                        <p className="topic">Aulas de Tutoria com profissionais da empresa</p>
+                    </div>
+                    <div className="ano-topic">
+                        <p className="ano">2021</p>
+                        <p className="topic">Academias de Negócio</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
