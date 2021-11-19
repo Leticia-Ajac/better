e951216554d74e2b2008f822f03f8168363a7d2d
Cayo <cayo.amaral@picpay.com>
2021-11-19 14:43:03 -0300

    alpdasjdclasdj

--- better/src/pages/qulture/contents/valores.jsx
+++ better/src/pages/qulture/contents/valores.jsx
@@ -4,13 +4,38 @@ const Valores = () => {
     return(
 		<>
 			<div id='quebrar'>
-				<div className="cards-valores" id="disciplina"><p className="titulo_valor">Disciplina</p></div>
-				<div className="cards-valores" id="umild"><p className="titulo_valor">Humildade</p></div>
-				<div className="cards-valores" id="determinacao"><p className="titulo_valor">Determinação</p></div>
-				<div className="cards-valores" id="disponivel"><p className="titulo_valor">Disponibilidade</p></div>
-				<div className="cards-valores" id="adono"><p className="titulo_valor">Atitude de dono</p></div>
-				<div className="cards-valores" id="minimalista"><p className="titulo_valor">Simplicidade</p></div>
-				<div className="cards-valores" id="franco"><p className="titulo_valor">Franqueza</p></div>
+				<div className="cards-valores" id="disciplina">
+					<div clasName="escuridao">
+						<p className="titulo_valor">Disciplina</p>
+					</div>
+				</div>
+				
+				<div className="cards-valores" id="umild">
+					<div className="escuridao">
+						<p className="titulo_valor">Humildade</p>
+					</div>
+				</div>
+
+				<div className="cards-valores" id="determinacao">
+					<p className="titulo_valor">Determinação</p>
+				</div>
+
+				<div className="cards-valores" id="disponivel">
+					<p className="titulo_valor">Disponibilidade</p>
+				</div>
+
+				<div className="cards-valores" id="adono">
+					<p className="titulo_valor">Atitude de dono</p>
+				</div>
+
+				<div className="cards-valores" id="minimalista">
+					<p className="titulo_valor">Simplicidade</p>
+				</div>
+
+				<div className="cards-valores" id="franco">
+					<p className="titulo_valor">Franqueza</p>
+				</div>
+				
 				<div id='img_observadora'>AAA</div>
 			</div>
 		</>
