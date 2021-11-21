6e5e5594b0e3a106195e865ffc1b86ba402fd431
gabriel macegosso <gmacegosso@gmail.com>
2021-11-21 20:45:18 -0300

    commit-content-lgbt

--- better/src/pages/qulture/contents/lgbt.jsx
+++ better/src/pages/qulture/contents/lgbt.jsx
@@ -2,7 +2,38 @@ import '../styles/lgbt.css'
 
 const Lgbt = () => {
     return (
-        <div className='ajuste1'>
+        <div className='Lgpd-page'>
+            <div className="titulo">
+                <img/>
+                <h1>LGPD</h1>
+                <img/>
+            </div>
+            <div className="container-textos">
+                <p>O compromisso da Escola Germinare com a Privacidade e Proteção de Dados.</p>
+                <p>Se você utiliza o nosso site, esta Política de Privacidade se aplica a “VOCÊ”.</p>
+            </div>
+            <div className="colunas-lgpd">
+                <div className="coluna-1-lgpd">
+                    <h1>Dados coletados no processo  seletivo de admissão</h1>
+                    <p>Nome
+                        Nome Social
+                        E-mail
+                        Sexo
+                        Data de Nascimento
+                        Nº do CPF
+                        Nº do RG
+                            Órgão Expedidor do RG
+                        Endereço
+                        Escola que está macritulado 
+                            Período
+                            Tipo de Instituição
+                    </p>
+                </div>
+                <div className="coluna-2-lgpd">
+                <h1>Cookies</h1>
+                    <p>Cookies estão inclusos em nossos sites para distinguir usuários únicos, atribuindo um número gerado  aleatoriamente como um identificador com o objetivo de calcular acessos dos visitantes para os relatórios analíticos de sites e para poder entender melhor duas necessidades e interesses. Oferecendo um melhor serviço ou fornecendo informação relacionada. Por padrão, ele é definido para expirar.</p>
+                </div>
+            </div>
         </div>
     )
 }
