13e1e45ba66434a9a1204950e238354efdbff922
gabriel macegosso <gmacegosso@gmail.com>
2021-11-24 13:29:33 -0300

    links certos de demonstrativos

--- better/src/pages/qulture/contents/demonstrativos.jsx
+++ better/src/pages/qulture/contents/demonstrativos.jsx
@@ -11,14 +11,14 @@ const Demonstrativos = () => {
             <div id='demonstrativos-cards'>
                 <a className='demonstrativos-anos' target='_blank'  href='http://www.escolagerminare.org.br/pdf/2010.pdf'>Demostrativo 2010</a>
                 <a className='demonstrativos-anos' target='_blank'  href='http://www.escolagerminare.org.br/pdf/2011.pdf'>Demostrativo 2011</a>
-                <a className='demonstrativos-anos' target='_blank'  href='http://www.escolagerminare.org.br/wp-content/uploads/2015/08/Demonstrativo-Instit uhref2012.pdf'>Demostrativo 2012</a>
+                <a className='demonstrativos-anos' target='_blank'  href='http://www.escolagerminare.org.br/wp-content/uploads/2015/08/Demonstrativo-Instituto2012.pdf'>Demostrativo 2012</a>
                 <a className='demonstrativos-anos' target='_blank'  href='http://www.escolagerminare.org.br/pdf/2013.pdf'>Demostrativo 2013</a>
                 <a className='demonstrativos-anos' target='_blank'  href='http://www.escolagerminare.org.br/pdf/2014.pdf'>Demostrativo 2014</a>
-                <a className='demonstrativos-anos' target='_blank'  href='http://www.escolagerminare.org.br/wp-content/uploads/2015/08/152593-Demonstra%C3%A7%C3%B5es-financeiras-31-12-2015-Instit uhref-Germinare_CLIENTE.pdf'>Demostrativo 2015</a>
-                <a className='demonstrativos-anos' target='_blank'  href='http://www.escolagerminare.org.br/wp-content/uploads/2015/08/Demonstrac%CC%A7o%CC%83es-financeiras-31-12-2016-Instit uhref-Germinare-com-parecer.pdf'>Demostrativo 2016</a>
-                <a className='demonstrativos-anos' target='_blank'  href='http://www.escolagerminare.org.br/wp-content/uploads/2015/08/demonstrativo_Instit uhref-Germinare.pdf'>Demostrativo 2017</a>
-                <a className='demonstrativos-anos' target='_blank'  href='http://www.escolagerminare.org.br/wp-content/uploads/2015/08/demonstrativo_Instit uhref-Germinare.pdf'>Demostrativo 2018</a>
-                <a className='demonstrativos-anos' target='_blank'  href='http://www.escolagerminare.org.br/wp-content/uploads/2015/08/demonstrativo_Instit uhref-Germinare.pdf'>Demostrativo 2019</a>
+                <a className='demonstrativos-anos' target='_blank'  href='http://www.escolagerminare.org.br/wp-content/uploads/2015/08/152593-Demonstra%C3%A7%C3%B5es-financeiras-31-12-2015-Instituto-Germinare_CLIENTE.pdf'>Demostrativo 2015</a>
+                <a className='demonstrativos-anos' target='_blank'  href='http://www.escolagerminare.org.br/wp-content/uploads/2015/08/Demonstrac%CC%A7o%CC%83es-financeiras-31-12-2016-Instituto-Germinare-com-parecer.pdf'>Demostrativo 2016</a>
+                <a className='demonstrativos-anos' target='_blank'  href='http://www.escolagerminare.org.br/wp-content/uploads/2015/08/demonstrativo_Instituto-Germinare.pdf'>Demostrativo 2017</a>
+                <a className='demonstrativos-anos' target='_blank'  href='http://www.escolagerminare.org.br/wp-content/uploads/2015/08/demonstrativo_Instituto-Germinare.pdf'>Demostrativo 2018</a>
+                <a className='demonstrativos-anos' target='_blank'  href='http://www.escolagerminare.org.br/wp-content/uploads/2015/08/demonstrativo_Instituto-Germinare.pdf'>Demostrativo 2019</a>
                 <a className='demonstrativos-anos' target='_blank'  href='http://www.escolagerminare.org.br/wp-content/uploads/2015/08/1171-21_Relatorio-de-Auditoria-_Germinare_2020-3.pdf'>Demostrativo 2020</a>
             </div>
             <p className='explicacao-demonstrativos' id='to-be-black'>Publicamos nosso demonstrativos financeiros como forma de manter a transparência junto a sociedade e apoiadores da Escola Germinare.</p>
